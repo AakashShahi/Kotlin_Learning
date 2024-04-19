64893a6aaadcb1849215c9b0a7ce8f46a1e1cd91
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-04-19 12:43:20 +0545

    chnages

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var radioApple: RadioButton
+    lateinit var radioSamsung: RadioButton
+    lateinit var radioOppo: RadioButton
+    lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        radioApple=findViewById(R.id.btnApple)
+        radioSamsung=findViewById(R.id.btnSamsung)
+        radioOppo=findViewById(R.id.btnOppo)
+        image=findViewById(R.id.imageView2)
+
+        radioApple.setOnClickListener {
+           image.setImageResource(R.drawable.download)
+        }
+        radioSamsung.setOnClickListener {
+           image.setImageResource(R.drawable.images)
+        }
+        radioOppo.setOnClickListener {
+           image.setImageResource(R.drawable.oppo)
+        }
+
     }
 }
